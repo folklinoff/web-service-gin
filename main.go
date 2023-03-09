71a6c5964183ce2715283cbf589ff59f99e5a5dd
Maksud <leylakarimova@mail.ru>
2023-03-09 22:43:41 +0300

    Added a few games (CS:GO, Minecraft, Skyrim)

--- main.go
+++ main.go
@@ -6,4 +6,9 @@ func main() {
 		Publisher{name: "Mojang", year: 2009},
 		Publisher{name: "Bethesda Game Studios", year: 2001},
 	}
+	var games []Game = []Game{
+		Game{name: "CS:GO", genre: "FPS", year: 2001, publisher: &publishers[0]},
+		Game{name: "Minecraft", genre: "Sandbox", year: 2009, publisher: &publishers[1]},
+		Game{name: "Skyrim", genre: "Roleplay Fighting", year: 2011, publisher: &publishers[2]},
+	}
 }
