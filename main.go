1ee81793559adf127caf8fab118c49676d3d9a68
Maksud <leylakarimova@mail.ru>
2023-03-09 22:27:42 +0300

    Added Game struct

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+type Game struct {
+	name      string     `json:"name"`
+	genre     string     `json:"genre"`
+	year      int16      `json:"year"`
+	publisher *Publisher `json:"Publisher"`
+}
+
 func main() {
 
 }
