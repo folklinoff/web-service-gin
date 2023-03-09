e8558288bd23b173d8c10f6c089ee0a2f9cf83ad
Maksud <leylakarimova@mail.ru>
2023-03-09 22:51:49 +0300

    publishers and games are now global

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
+var publishers []Publisher = []Publisher{
+	Publisher{name: "Valve", year: 1996},
+	Publisher{name: "Mojang", year: 2009},
+	Publisher{name: "Bethesda Game Studios", year: 2001},
+}
+var games []Game = []Game{
+	Game{name: "CS:GO", genre: "FPS", year: 2001, publisher: &publishers[0]},
+	Game{name: "Minecraft", genre: "Sandbox", year: 2009, publisher: &publishers[1]},
+	Game{name: "Skyrim", genre: "Roleplay Fighting", year: 2011, publisher: &publishers[2]},
+}
+
 func getGames(c *gin.Context) {
 	c.IndentedJSON(https.StatusOK, games)
 }
 
 func main() {
-	var publishers []Publisher = []Publisher{
-		Publisher{name: "Valve", year: 1996},
-		Publisher{name: "Mojang", year: 2009},
-		Publisher{name: "Bethesda Game Studios", year: 2001},
-	}
-	var games []Game = []Game{
-		Game{name: "CS:GO", genre: "FPS", year: 2001, publisher: &publishers[0]},
-		Game{name: "Minecraft", genre: "Sandbox", year: 2009, publisher: &publishers[1]},
-		Game{name: "Skyrim", genre: "Roleplay Fighting", year: 2011, publisher: &publishers[2]},
-	}
 }
