c274f39d4c55980da6041d0eb2135bbeb61a7e76
Maksud <leylakarimova@mail.ru>
2023-03-09 22:38:20 +0300

    Added a few publishers (Valve, Mojand, Bethesda)

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
 func main() {
-
+	var publishers []Publisher = []Publisher{
+		Publisher{name: "Valve", year: 1996},
+		Publisher{name: "Mojang", year: 2009},
+		Publisher{name: "Bethesda Game Studios", year: 2001},
+	}
 }
