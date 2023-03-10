ed9610cfd0f05d699d6def40c0b2fe8e7be916d1
Maksud <leylakarimova@mail.ru>
2023-03-10 15:51:07 +0300

    Added and used getGameByName function

--- main.go
+++ main.go
@@ -17,6 +17,22 @@ func postGames(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newGame)
 }
 
+func getGameByName(c *gin.Context) {
+	name := c.Param("name")
+
+	for _, a := range games {
+		if a.Name == name {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "game not found"})
+}
+
+func getGames(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, games)
+}
+
 var publishers []Publisher = []Publisher{
 	{Name: "Valve", Year: 1996},
 	{Name: "Mojang", Year: 2009},
@@ -29,13 +45,10 @@ var games []Game = []Game{
 	{Name: "Skyrim", Genre: "Roleplay Fighting", Year: 2011, Publisher: &publishers[2]},
 }
 
-func getGames(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, games)
-}
-
 func main() {
 	router := gin.Default()
 	router.GET("/games", getGames)
+	router.GET("/games/:name", getGameByName)
 	router.POST("/games", postGames)
 
 	router.Run("localhost:8080")
